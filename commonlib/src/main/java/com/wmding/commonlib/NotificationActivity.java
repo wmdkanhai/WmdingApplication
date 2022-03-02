c2695839b38fb8a2d5db6d8d74df827c6b0262cb
wmding <>
2022-03-02 21:42:13 +0800

    feat：Notification进阶

--- commonlib/src/main/java/com/wmding/commonlib/NotificationActivity.java
+++ commonlib/src/main/java/com/wmding/commonlib/NotificationActivity.java
@@ -3,10 +3,13 @@ package com.wmding.commonlib;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,6 +25,11 @@ public class NotificationActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void btn1(View view) {
+
+        // 添加跳转页面
+        Intent intent = new Intent(this, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         String id = "MyNotification_channel";
@@ -38,8 +46,44 @@ public class NotificationActivity extends AppCompatActivity {
                 .setWhen(System.currentTimeMillis())
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
                 .build();
         notificationManager.notify(1, notification);
 
+    }
+
+    public void btn2(View view) {
+        // 添加跳转页面
+        Intent intent = new Intent(this, NotificationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        String id = "MyNotification_channel";
+        String name = "MyNotification";
+        Notification notification;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        String contentText = "this is content text this is content text this is content text this is content text this is content textthis is content text";
+        notification = new NotificationCompat.Builder(this, id)
+                .setContentTitle("this is content title")
+//                .setContentText(contentText)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pendingIntent)
+
+                // 设置通知的重要程度
+                .setPriority(NotificationManagerCompat.IMPORTANCE_MAX)
+                .setAutoCancel(true)
+
+                // 设置长文本样式
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(contentText))
+                .build();
+        notificationManager.notify(1, notification);
     }
 }
